16aed58bd9de1a1dddb821b5dff4a51eed265b48
AceSpadeAble <wygoda.pawel@gmail.com>
2024-01-31 19:45:14 +0100

    created arrayMap

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 console.log('START')
 
+// String part
 let map = '\n'
 
 for (let x = 0; x<9; x++) {
@@ -14,5 +15,22 @@ for (let x = 0; x<9; x++) {
 }
 
 console.log('Map:', map)
+
+// Space can become three tiles:
+// SEA - 'S', COAST - 'C', LAND - 'L'
+
+// Array Part
+// First I need to add posibilities for each space
+let arrayMap = [] 
+
+for (let x = 0; x<9; x++) {
+    arrayMap[x] = []
+
+    for (let y = 0; y<9; y++) {
+        arrayMap[x][y] = ['S','C','L']
+    }
+}
+
+console.log('arrayMap - ', arrayMap)
 
 console.log('END')
