2b9d53cf0720eefb8786ca7f239bc0e7acaa8385
Paweł Wygoda <wygoda.pawel@gmail.com>
2024-04-04 14:37:47 +0200

    added html and css files

--- main.js
+++ main.js
@@ -4,14 +4,18 @@ import { getRandomNumber } from "./optionFunctions.js"
 const numberOfoptions = 3
 let worldMap = gridCreator()
 
+function draw() {
+    const canvas = document.getElementById("tutorial");
+    if (canvas.getContext) {
+      const ctx = canvas.getContext("2d");
+    }
+  }
+  window.addEventListener("load", draw);
 
+// // ??
+// worldMap[getRandomNumber(worldMap.length)].option = [getRandomNumber(numberOfoptions)]
+// worldMap[getRandomNumber(worldMap.length)].isCollapsed = true
+// // console.log(worldMap)
+// for (let i = 0; i<worldMap.length; i++) {
 
-worldMap[getRandomNumber(worldMap.length)].option = [getRandomNumber(numberOfoptions)]
-
-
-console.log(worldMap)
-
-
-for (let i = 0; i<worldMap.length; i++) {
-
-}
+// }
