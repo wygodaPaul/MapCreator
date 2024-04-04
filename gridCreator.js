1d0e4a44f5a303c11ff3a6ecefeacde92c773571
Paweł Wygoda <wygoda.pawel@gmail.com>
2024-04-04 17:06:10 +0200

    made gridCreator

--- gridCreator.js
+++ gridCreator.js
@@ -1,10 +1,16 @@
-
-export const gridCreator = () => {
-const grid = 3
+import * as THREE from 'three';
+export const gridCreator = (grid) => {
 let map = []
+let arrayOfPlanes = []
 
 for (let i = 0; i < grid; i++) {
     for (let j = 0; j < grid; j++) {
+        let planeMaterial = new THREE.MeshBasicMaterial({color: "grey"})
+        let planeGeometry = new THREE.BoxGeometry( 9, 1, 9 )
+        let plane = new THREE.Mesh( planeGeometry, planeMaterial )
+        plane.position.x = 10 * i
+        plane.position.y = 0
+        plane.position.z = 10 * j
         let cell = {
             row : i,
             column : j,
@@ -12,11 +18,12 @@ for (let i = 0; i < grid; i++) {
             option: [0, 1, 2]
         }
         map.push(cell)
+        arrayOfPlanes.push(plane)
     }
 }
 
 
-// console.log(map)
-return map
+console.log(arrayOfPlanes)
+return  arrayOfPlanes 
 }
 
--- main.js
+++ main.js
@@ -1,8 +1,45 @@
-import {gridCreator} from "./gridCreator.js"
+import * as THREE from 'three';
+
+import { gridCreator } from "./gridCreator.js"
 import { getRandomNumber } from "./optionFunctions.js"
 
+const grid = 5
 const numberOfoptions = 3
-let worldMap = gridCreator()
+let  arrayOfPlanes  = gridCreator(grid)
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+// const geometry = new THREE.BoxGeometry( 10, 10, 10 );
+// const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+// const cube = new THREE.Mesh( geometry, material );
+// scene.add( cube );
+for (let i = 0; i< arrayOfPlanes.length; i++) {
+    console.log('PING')
+    scene.add( arrayOfPlanes[i] )
+}
+
+camera.position.x = grid*10/2-5;
+camera.position.y = grid*10;
+camera.position.z = grid*10/2-5;
+camera.lookAt( grid*10/2-5, 0, grid*10/2-5 )
+
+function animate() {
+	requestAnimationFrame( animate );
+
+	// cube.rotation.x += 0.01;
+	// cube.rotation.y += 0.01;
+
+	renderer.render( scene, camera );
+}
+
+animate();
+
+
 
 
 // // ??
