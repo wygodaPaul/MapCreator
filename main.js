3a79bf7c3b76f346b07ef801d566125da3129e86
Paweł Wygoda <wygoda.pawel@gmail.com>
2024-04-11 20:39:28 +0200

    creating function for checking other cells options

--- main.js
+++ main.js
@@ -29,35 +29,70 @@ const options = [
     { tile: 'coast', tile_ID: 1, color: "yellow", rules: {top: [0, 1], right: [1, 2], bottom: [1, 2], left: [0, 1]}  },
     { tile: 'sea', tile_ID: 2, color: "blue", rules: {top: [1, 2], right: [2], bottom: [2], left: [1, 2]}  }
 ]
-    let digit = getRandomNumber(worldMap.length)
-    worldMap[digit].option = [getRandomNumber(3)]
-    worldMap[digit].isCollapsed = true
+    // let digit = getRandomNumber(worldMap.length)
+    // worldMap[digit].option = [getRandomNumber(3)]
+    // worldMap[digit].isCollapsed = true
 
-	digit = getRandomNumber(worldMap.length)
-    worldMap[digit].option = [getRandomNumber(3)]
-    worldMap[digit].isCollapsed = true
+	// digit = getRandomNumber(worldMap.length)
+    // worldMap[digit].option = [getRandomNumber(3)]
+    // worldMap[digit].isCollapsed = true
+
+	// const pickRandom = () => {
+	// 	let planeMaterial = new THREE.MeshBasicMaterial({color: options[worldMap[digit].option].color})
+	// 	let planeGeometry = new THREE.BoxGeometry( 8.9, 1, 8.9 )
+	// 	let plane = new THREE.Mesh( planeGeometry, planeMaterial )
+	// 	plane.position.x = 10 * worldMap[digit].row
+	// 	plane.position.y = 1
+	// 	plane.position.z = 10 * worldMap[digit].column
+	// 	scene.add( plane )
+	// }
+
+const test = () => {
+	console.log('currentCell - ', worldMap[0 + 1 * grid])
+
+	console.log('otherCell - ', worldMap[1 + 1 * grid])
+
+}
+
+test()
 
 //Find lowest entropy
 const findLowest = () => {
 	let tempArray = worldMap.sort((a, b) => a.option.length - b.option.length)
 	let myArray = tempArray.filter(x => tempArray[0].option.length === x.option.length)
-	console.log("hello - ", myArray)
-
 	let randomCell = myArray[getRandomNumber(myArray.length)]
 	console.log("randomCell - ", randomCell)
-	// pickRANDOM
+	return randomCell
 }
 
 findLowest()
-// const pickRandom = () => {
-// 	let planeMaterial = new THREE.MeshBasicMaterial({color: options[worldMap[digit].option].color})
-// 	let planeGeometry = new THREE.BoxGeometry( 8.9, 1, 8.9 )
-// 	let plane = new THREE.Mesh( planeGeometry, planeMaterial )
-// 	plane.position.x = 10 * worldMap[digit].row
-// 	plane.position.y = 1
-// 	plane.position.z = 10 * worldMap[digit].column
-// 	scene.add( plane )
-// }
+
+const checkNeighbors = (currentCell) => {
+	let allPossibleOptions = [0, 1, 2]
+	let otherCellAvaliableOptions
+	// Check Top
+	if (currentCell.row > 0) {
+		otherCellAvaliableOptions = worldMap[currentCell.column + (currentCell.row - 1) * grid]
+	}
+
+	// Check Right
+	if (currentCell.column < grid - 1) {
+		otherCellAvaliableOptions = worldMap[(currentCell.column + 1) + currentCell.row * grid]
+	}
+
+	// Check Bottom
+	if (currentCell.row < grid - 1) {
+		otherCellAvaliableOptions = worldMap[currentCell.column + (currentCell.row + 1) * grid]
+
+	}
+
+	// Check Left
+	if (currentCell.column > 0) {
+		otherCellAvaliableOptions = worldMap[(currentCell.column - 1) + currentCell.row * grid]
+
+	}
+}
+
 
 const draw = () => {
 	for(let row = 0; row < grid; row++) {
